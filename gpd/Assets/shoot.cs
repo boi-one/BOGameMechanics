3c26ffec0f8961d5bdbe5b5496137587cc2ecf82
unknown <30570@ma-web.nl>
2022-03-25 12:26:55 +0100

    shooting

--- gpd/Assets/shoot.cs
+++ gpd/Assets/shoot.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class shoot : MonoBehaviour
 {
     public Transform prefab;
-
+    public int speed = 10000;
+    private Transform playertransform;
+    private Transform cameratransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playertransform = GameObject.Find("player").transform;
+        cameratransform = GameObject.Find("Main Camera").transform;
     }
 
     // Update is called once per frame
@@ -18,8 +21,12 @@ public class shoot : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             Debug.Log("klik");
-            Instantiate(prefab);
-            prefab.transform.position = transform.position;
+            Transform bullet = Instantiate(prefab);
+            bullet.transform.position = transform.position;
+            Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            Vector3 vel = (Quaternion.Euler(cameratransform.eulerAngles.x, playertransform.eulerAngles.y, 0) * Vector3.forward * speed);
+            //vel.z = 0;
+            rb.AddForce (vel);
         }
     }
 }
