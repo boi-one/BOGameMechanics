ff48d181131a81e1d67bc1deff3f4bea4b5c301d
unknown <30570@ma-web.nl>
2022-04-21 09:03:38 +0200

    zwaar betere animatie

--- gpd/Assets/animationcontroller.cs
+++ gpd/Assets/animationcontroller.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class animationcontroller : MonoBehaviour
 {
     public int down;
+    private bool klik;
     
     // Start is called before the first frame update
     void Start()
@@ -15,13 +16,27 @@ public class animationcontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !klik)
         {
-            transform.Rotate(down * Time.deltaTime, 0, 0);
+            StartCoroutine(bruh());
+            
+            
         }
-        if (Input.GetMouseButtonUp(0))
+
+    }
+    IEnumerator bruh()
+    {
+        klik = true;
+        for (int i = 0; i < 15; i++)
         {
+            yield return new WaitForSeconds(0.02f);
+            transform.Rotate(down * Time.deltaTime, 0, 0);    
+        }
+        for (int i = 0; i < 15; i++)
+        {
+            yield return new WaitForSeconds(0.02f);
             transform.Rotate(-down * Time.deltaTime, 0, 0);
         }
+        klik = false;
     }
 }
